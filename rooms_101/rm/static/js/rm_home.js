ea40cd21a203cd8dce9fc573e8709fed60513419
tmushayahama <tmushayahama@gmail.com>
2013-04-09 07:59:59 -0400

    Added a popup for the assign goal

---
+++ rooms_101/rm/static/js/rm_home.js
@@ -0,0 +1,28 @@
+// ________________________________________________________________
+// |-------------------------INITIALIZATIONS-----------------------|
+// `````````````````````````````````````````````````````````````````
+
+$(document).ready(function (e) {
+    console.log("Loading rm.js...");
+    $.ajaxSetup({traditional: true});
+    populateGoals();
+    addEventHandlers();
+});
+function populateGoals () {
+    for(var i=0; i<goals.length; i++) {
+    $("#rm-goals-home")
+        .append($("<li/>")
+                .append($("<a/>")
+                        .text(goals[i]["task_name"])));
+    }
+}
+function addEventHandlers() {
+    $('#rm-post-tab a').click(function (e) {
+        console.log("tab clicked");
+        e.preventDefault();
+        $(this).tab('show');
+    });
+     $('.rm-stop-propagation').click(function (e) {
+        e.stopPropagation();
+    });
+}
